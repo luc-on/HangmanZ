8bc078e67d7bfcdd39698d88afa7bee23e44a3b8
luc-on <jean.casaubon.b@gmail.com>
2023-07-16 00:53:38 -0300

    Progress 2

--- scripts/script.js
+++ scripts/script.js
@@ -1,56 +1,53 @@
 function startGame() {
-    // Activates the game after clicking on "Play"
+    // Prepares the game after clicking on "Play"
+    document.getElementById("main-language").style.cssText = "display: none";
     document.getElementById("main-logo").style.cssText = "display: none";
     document.getElementById("main-welcome").style.cssText = "display: none";
     document.getElementById("main-game").style.cssText = "display: grid";
+    getInput();
 };
 
-function testy(event, word_to_guess, raw_answer, score, wrongs) {
-    var name = event.key.toString().toLowerCase();
-    var abc = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-    if (word_to_guess.includes(name)) {
-        for (i = 0; i<word_to_guess.length; i++) {
-            if (word_to_guess[i] == name) {
-                raw_answer[i] = name;
-            }
-        };
-        displayAnswer(raw_answer);
-        if (raw_answer.join("") == word_to_guess) {
-            document.getElementById("game-header").innerHTML = "You won!";
-            terminateEvent();
-        }
-    } else if (abc.includes(name) && !(wrongs.includes(name))) {
-        wrongs.push(name);
-        addMistake(wrongs);
-        score.innerHTML++;
-        if (score.innerHTML == 7) {
-            score.innerHTML = "Game Over. You lost."
-        };
-    } else if (wrongs.includes(name)) {
-        document.getElementById("game-header").innerHTML = "Try another letter!"
-    } else {
-        document.getElementById("game-header").innerHTML = "Only letters, please!"
-    }
-  }
-
 function getInput() {
-    // Starts level after clicking on "Start"
-    var word_to_guess = wordGenerator();
+    // Starts up the game
+    word_to_guess = wordGenerator();
     var raw_answer = Array(word_to_guess.length).fill("_");
     displayAnswer(raw_answer);
     var score = document.getElementById("game-figure");
     var wrongs = [];
-    document.addEventListener('keydown', testy('keydown', word_to_guess, raw_answer, score, wrongs), false);
+    document.addEventListener('keydown', function(event) {
+        var name = event.key.toString().toLowerCase();
+        var abc = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+        if (word_to_guess.includes(name)) {
+            for (i = 0; i<word_to_guess.length; i++) {
+                if (word_to_guess[i] == name) {
+                    raw_answer[i] = name;
+                }
+            };
+            displayAnswer(raw_answer);
+            if (raw_answer.join("") == word_to_guess) {
+                document.getElementById("game-header-status").innerHTML = "You won!";
+                terminateEvent();
+            }
+        } else if (abc.includes(name) && !(wrongs.includes(name))) {
+            wrongs.push(name);
+            addMistake(wrongs);
+            score.innerHTML++;
+            if (score.innerHTML == 7) {
+                gameOver();
+            };
+        } else if (wrongs.includes(name)) {
+            document.getElementById("game-header-status").innerHTML = "Try another letter!"
+        } else {
+            document.getElementById("game-header-status").innerHTML = "Only letters, please!"
+        }    
+    }, false);
+    document.getElementById("game-giveup").addEventListener("click", gameOver)
     // removeEventListener
-}
-
-function terminateEvent() {
-    // document.removeEventListener('keydown', )
 }
 
 function addMistake(list) {
     // Adds incorrect guesses into a list
-    document.getElementById("game-info").innerHTML = list.join();
+    document.getElementById("game-wrongs").innerHTML = list.join();
 };
 
 function wordGenerator() {
@@ -65,7 +62,8 @@ function displayAnswer(word) {
     document.getElementById("game-attempts").innerHTML = word.join(" ")
 }
 
-function giveUp() {
+function gameOver() {
     // Reveals correct answer
-    document.getElementById("game-figure").innerHTML = "Game over. The answer was XXX";   
+    document.getElementById("game-header-status").innerHTML = `Game over. The answer was ${word_to_guess}`;
+    document.getElementById("game-figure").innerHTML = "x";
 };
