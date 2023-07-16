36a22d3b0039de557b4c38196bba04ffc010e71b
luc-on <jean.casaubon.b@gmail.com>
2023-07-16 01:16:15 -0300

    Playable

--- scripts/script.js
+++ scripts/script.js
@@ -9,40 +9,53 @@ function startGame() {
 
 function getInput() {
     // Starts up the game
+    document.getElementById("game-hint").style.cssText = "display: block";
+    document.getElementById("game-giveup").style.cssText = "display: block";
+    document.getElementById("game-replay").style.cssText = "display: none";
+    document.getElementById("game-header-status").innerHTML = "";
     word_to_guess = wordGenerator();
-    var raw_answer = Array(word_to_guess.length).fill("_");
+    raw_answer = Array(word_to_guess.length).fill("_");
     displayAnswer(raw_answer);
-    var score = document.getElementById("game-figure");
-    var wrongs = [];
-    document.addEventListener('keydown', function(event) {
-        var name = event.key.toString().toLowerCase();
-        var abc = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-        if (word_to_guess.includes(name)) {
-            for (i = 0; i<word_to_guess.length; i++) {
-                if (word_to_guess[i] == name) {
-                    raw_answer[i] = name;
-                }
-            };
-            displayAnswer(raw_answer);
-            if (raw_answer.join("") == word_to_guess) {
-                document.getElementById("game-header-status").innerHTML = "You won!";
-                terminateEvent();
-            }
-        } else if (abc.includes(name) && !(wrongs.includes(name))) {
-            wrongs.push(name);
-            addMistake(wrongs);
-            score.innerHTML++;
-            if (score.innerHTML == 7) {
-                gameOver();
-            };
-        } else if (wrongs.includes(name)) {
-            document.getElementById("game-header-status").innerHTML = "Try another letter!"
-        } else {
-            document.getElementById("game-header-status").innerHTML = "Only letters, please!"
-        }    
-    }, false);
+    score = document.getElementById("game-figure");
+    score.innerHTML = 0;
+    document.getElementById("game-wrongs").innerHTML = "";
+    wrongs = [];
+    document.addEventListener('keydown', xyz, false);
     document.getElementById("game-giveup").addEventListener("click", gameOver)
     // removeEventListener
+}
+
+function xyz() {
+    var name = event.key.toString().toLowerCase();
+    var abc = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    if (word_to_guess.includes(name)) {
+        for (i = 0; i<word_to_guess.length; i++) {
+            if (word_to_guess[i] == name) {
+                raw_answer[i] = name;
+            }
+        };
+        displayAnswer(raw_answer);
+        if (raw_answer.join("") == word_to_guess) {
+            document.getElementById("game-header-status").innerHTML = "You won!";
+            document.getElementById("game-hint").style.cssText = "display: none";
+            document.getElementById("game-giveup").style.cssText = "display: none";
+            document.getElementById("game-replay").style.cssText = "display: block";
+            document.getElementById("game-points").innerHTML ++;
+            document.removeEventListener('keydown', xyz);
+            document.getElementById("game-replay").addEventListener("click", getInput);
+        }
+    } else if (abc.includes(name) && !(wrongs.includes(name))) {
+        wrongs.push(name);
+        addMistake(wrongs);
+        score.innerHTML++;
+        if (score.innerHTML == 7) {
+            gameOver();
+        };
+    } else if (wrongs.includes(name)) {
+        document.getElementById("game-header-status").innerHTML = "Try another letter!"
+    } else {
+        document.getElementById("game-header-status").innerHTML = "Only letters, please!"
+    }    
 }
 
 function addMistake(list) {
